a0b417a4a900360ddd111b5f8f3c75a9adec7a43
jerome moliere <jerome@javaxpert.com>
2024-07-23 17:36:27 +0200

    makes dependencyCheckFail

--- src/main/java/org/example/app/MainApp.java
+++ src/main/java/org/example/app/MainApp.java
@@ -4,10 +4,14 @@ import io.vavr.collection.List;
 import org.example.domain.adapters.MQRelatedClass;
 import org.example.infrastructure.DoSomethingWithMQ;
 
+import java.time.Instant;
+import java.util.Date;
+
 public class MainApp {
     public static void main(String[] args) {
         MQRelatedClass mqTool = new MQRelatedClass();
         mqTool.toString();
         List myList = List.of("1","2","3");
+        Date d = Date.from(Instant.now());
     }
 }
