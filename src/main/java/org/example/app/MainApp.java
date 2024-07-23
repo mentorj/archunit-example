6a91e43bb3cdd25397b7fe1b039e78d774673270
jerome moliere <jerome@javaxpert.com>
2024-07-23 16:34:19 +0200

    adds domain class dependencies check

--- src/main/java/org/example/app/MainApp.java
+++ src/main/java/org/example/app/MainApp.java
@@ -1,5 +1,6 @@
 package org.example.app;
 
+import io.vavr.collection.List;
 import org.example.domain.adapters.MQRelatedClass;
 import org.example.infrastructure.DoSomethingWithMQ;
 
@@ -7,5 +8,6 @@ public class MainApp {
     public static void main(String[] args) {
         MQRelatedClass mqTool = new MQRelatedClass();
         mqTool.toString();
+        List myList = List.of("1","2","3");
     }
 }
--- src/test/java/org/example/ArchitectureTest.java
+++ src/test/java/org/example/ArchitectureTest.java
@@ -4,6 +4,7 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 @AnalyzeClasses(packages = "org.example")
 public class ArchitectureTest {
@@ -19,4 +20,9 @@ public class ArchitectureTest {
     private static final ArchRule rule2 =
             classes().that().resideInAPackage("..adapters..")
                     .should().onlyBeAccessed().byAnyPackage("..app..");
+
+    @ArchTest
+    private static final ArchRule rule3 =
+            noClasses().that().resideInAPackage("..domain..")
+                    .should().dependOnClassesThat().resideInAPackage("com.google.guava..");
 }
