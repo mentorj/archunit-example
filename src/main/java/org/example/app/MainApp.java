12e55707228f8488e3bf7b788babb12753f43887
jerome moliere <jerome@javaxpert.com>
2024-07-23 16:03:06 +0200

    1st rule violated

--- src/main/java/org/example/app/MainApp.java
+++ src/main/java/org/example/app/MainApp.java
@@ -1,4 +1,11 @@
 package org.example.app;
 
+import org.example.infrastructure.DoSomethingWithMQ;
+
 public class MainApp {
+    public static void main(String[] args) {
+
+        DoSomethingWithMQ mq = new DoSomethingWithMQ();
+        mq.foobar();
+    }
 }
